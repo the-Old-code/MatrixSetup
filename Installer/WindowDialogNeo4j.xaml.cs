00b24ddb434dfd5b0b9aeda4028f9d595a79145f
the-Old-code <jijan1337@gmail.com>
2023-12-08 17:46:49 +0500

    Окно neo4j Добавление окна настройки установочного пути для neo4j Оно проверяет введенный путь и редактирует json файл конфигурации для web server(ConnectionStrings)

---
+++ Installer/WindowDialogNeo4j.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Installer
+{
+    /// <summary>
+    /// Interaction logic for WindowDialogNeo4j.xaml
+    /// </summary>
+    public partial class WindowDialogNeo4j : Window
+    {
+        public WindowDialogNeo4j()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_browsepath_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Forms.FolderBrowserDialog folderDialog = new System.Windows.Forms.FolderBrowserDialog();
+            folderDialog.ShowNewFolderButton = false;
+            folderDialog.SelectedPath = AppDomain.CurrentDomain.BaseDirectory;
+            System.Windows.Forms.DialogResult result = folderDialog.ShowDialog();
+
+            if (result == System.Windows.Forms.DialogResult.OK)
+            {
+                txtbox_path_Neo4j.Text = folderDialog.SelectedPath;
+            }
+        }
+
+        private void btn_OK_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtbox_path_Neo4j.Text.Contains(" ")) 
+            {
+                MessageBox.Show("В пути присутствуют пробелы");
+                return;
+            }
+            if (Directory.Exists(txtbox_path_Neo4j.Text))
+            {
+                InstallScenario.Neo4jInstallPath = txtbox_path_Neo4j.Text;
+                Close();
+            }
+            else if (txtbox_path_Neo4j.Text == "") Close();
+            else MessageBox.Show("Не существующая папка");
+        }
+    }
+}
